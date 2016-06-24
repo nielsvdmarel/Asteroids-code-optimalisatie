0b076cd8efae99435e7a2d94671a30d6b1f7bdbb
nielsvdmarel <nielsvdmarel@gmail.com>
2016-06-24 11:31:07 +0200

    camera follow script met comments

--- Assets/scripts/CamerFollow.cs
+++ Assets/scripts/CamerFollow.cs
@@ -10,6 +10,7 @@ public class CamerFollow : MonoBehaviour {
 	
 	void Start ()
     {
+        // component camera krijgen
         mycam = GetComponent<Camera>();
 	}
 
@@ -21,22 +22,26 @@ public class CamerFollow : MonoBehaviour {
 
         if (target)
         {
+            //zorgt ervoor dat de camera de target volgt, met een standaard z waarde van -10
             transform.position = Vector3.Lerp(transform.position, target.position, Camera_follow_speed) + new Vector3(0, 0, -10);
         }
 
-
+        // als e wordt ingedurkt zoomt de camera in
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (zoomSize > 2)
                 zoomSize -=1;
         }
-
+        
+        // als e wordt ingedurkt zoomt de camera uit
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if (zoomSize < 35)
                 zoomSize += 1;
 
         }
+
+
 
         GetComponent<Camera>().orthographicSize = zoomSize;
 
